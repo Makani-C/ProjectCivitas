b268a7a53200b9a29b9d05fb8b16e6d301e50485
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-08-22 13:34:44 -0700

    Initial Commit

---
+++ projectcivitas/projectcivitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectcivitasApp.swift
+//  projectcivitas
+//
+//  Created by Makani Cartwright on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct projectcivitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
